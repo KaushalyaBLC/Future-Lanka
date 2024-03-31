5e173e84c905cb62f01c063dd1b47c788f2b700b
Chamath Kaushalya <chamathkaushalya8@gmail.com>
2024-03-31 16:51:11 +0530

    made adjustments to backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -22,8 +22,6 @@ const questions = [
 
   "How comfortable are you with writing essays or reports? (Rate from 1 to 5)",
 
-  "Give an example of a time when you effectively communicated in a group setting. What was the outcome?",
-
   "What subjects are you most passionate about, and why?",
 
   "Describe a topic or concept you learned outside your school curriculum that fascinated you.",
@@ -32,25 +30,12 @@ const questions = [
 
   "Have you taken any initiatives or led any projects within these activities? Describe your role and the impact of your work.",
 
-  "What career path are you considering pursuing after A-Levels? Why does this particular path interest you?",
-
-  "How do you plan to achieve your career or academic goals? Do you have any steps or plans already in place?",
+  "Name 3 caeer path are you considering pursuing after A-Level exams? Why does this particular paths interest you?",
 
   "Do you prefer to work individually or as part of a team? Explain why",
 
   "Describe how you organize and prioritize your schoolwork and extracurricular activities.",
 
-  "Think of a problem you've recently solved or a challenge you've overcome. How did you approach it, and what was the solution?",
-
-  "How do you handle stress and pressure, especially during exam periods or significant projects?",
-
-  "Can you recall a time when you had to adapt to a significant change? How did you handle it?",
-
-  "How do you respond to criticism or feedback? Provide an example of how you used feedback to improve.",
-
-  "Describe a situation where you helped someone in a difficult situation. What did you do, and what was the result?",
-
-  "How do you build and maintain relationships with peers, teachers, and family?",
 ];
 
 const {
@@ -110,7 +95,7 @@ async function runChat(userMessage) {
         role: "model",
         parts: [
           {
-            text: "act like a career counselor using the following question and answers i give in next input write 3 paragraphs that describe me highlighting my passion and skills and personality (each paragraph must at least 100 words) then suggest me most suitable 5 jobs for me answer shold be like {paragraph1:[paragraph1], paragraph2:[paragraph2]}  {careertitle:title, description:description about the career, skills:[skills needed], salary-range:salary range in lkr if you dont satisfy the answers given dont answer and just say answers are not good enough to give a output politely",
+            text: "act like a career counselor using the following question and answers i give in next input write 3 paragraphs that describe me highlighting my passion and skills and personality (each paragraph must at least 100 words) then suggest me most suitable 5 jobs for me answer shold be like {paragraph1, paragraph2}  {careertitle:title, description:description about the career, skills:[skills needed], if you dont satisfy the answers given to following questions dont answer and just say answers are not good enough to give a output politely",
           },
         ],
       },
@@ -142,20 +127,12 @@ app.post("/api/submitFormData", async (req, res) => {
     ${questions[9]}-${formData.q10},
     ${questions[10]}-${formData.q11},
     ${questions[11]}-${formData.q12},
-    ${questions[12]}-${formData.q13},
-    ${questions[13]}-${formData.q14},
-    ${questions[14]}-${formData.q15},
-    ${questions[15]}-${formData.q16},
-    ${questions[16]}-${formData.q17},
-    ${questions[17]}-${formData.q18},
-    ${questions[18]}-${formData.q19},
-    ${questions[19]}-${formData.q20},
     
-    im a srilankan student using these questions and answers describe me as long as you can and suggest me 6 careers paragrahs must be natural language and be descriptive and long as you can job suggestion mus have a descriptio technical skills,soft skills and salary range finally give me some advice to match up with the job market  `;
+    im a srilankan student using these questions and answers describe me as long as you can and suggest me 6 careers paragrahs must be natural language and be descriptive and long as you can job suggestion mus have a descriptio technical skills,soft skills finally give me some advice to match up with the job market  `;
 
   await runChat(prompt);
   const cleanedResponse = rep.replace(/\*\*/g, "");
-  console.log("Reply generated!");
+  console.log(`Reply generated! - ${formData.name}`);
 
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
